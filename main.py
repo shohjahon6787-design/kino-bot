4b390eac4a77eb79af826209e84574f6c69cfc16
shohjahon6787-design <shohjahon.6787@gmail.com>
2025-12-19 16:31:13 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
 
-TOKEN = os.getenv("8525356182:AAEZCjDjylYYxXgOrIv0wIjLBKCUTwBSHDI")
+TOKEN = os.getenv("8525356182:AAGilZbri8O9yoFs-TBztDR4yDRkVY8zcp8")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
